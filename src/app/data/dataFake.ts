06bf6113d051bfe35896e4ceec778766f08476f1
Vinicius <Viniciussouzaalves@gmail.com>
2023-05-27 18:45:56 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Guardiões da Galáxia Vol.3",
+    "description": "Peter Quill reune sua equipe pra combater novos vilões",
+    "photoCover": "https://cdn.marvel.com/content/1x/poster_6328209281112_32ba2a7f6217bd81951abf1abc3fb220.jpg"
+  },
+
+  {
+    "id": "2",
+    "title": "Marvel Studios anuncia novas datas de estreia para 'Loki' e 'Echo'",
+    "description": "Marvel Studios anuncia novas datas de estreia para 'Loki' e 'Echo'",
+    "photoCover": "https://cdn.marvel.com/content/1x/studios.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Revisite os Guardiões da Galáxia com 'Marvel Studios' Legends' no Disney+",
+    "description": "Revisite os Guardiões da Galáxia com 'Marvel Studios' Legends' no Disney+",
+    "photoCover": "https://i.ytimg.com/vi/HefzNnbUQJM/maxresdefault.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Cobie Smulders é a estrela convidada no último episódio de 'Marvel's Moon Girl and Devil Dinosaur'",
+    "description": "Cobie Smulders",
+    "photoCover": "https://i.ytimg.com/vi_webp/jaLkjZFKx6s/maxresdefault.webp"
+  }
+]
